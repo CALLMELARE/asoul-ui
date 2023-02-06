2d11cccee185730939d478302abdf52e7a78c118
左云飞 <yunfei@tju.edu.cn>
2023-02-06 11:46:48 +0800

    [feat]主题方法

---
+++ packages/utils/theme/presetThemes.ts
@@ -0,0 +1,10 @@
+const presetThemes = {
+  default: {},
+  ava: {
+    "--asoul-theme-color": "#9ac8e2",
+    "--asoul-primary-color": "#252525",
+    "--asoul-secondary-color": "#656667",
+    "--asoul-sub-color": "#cccccc",
+    "--asoul-invert-color": "#ffffff",
+  },
+};
--- packages/utils/theme/useTheme.ts
+++ packages/utils/theme/useTheme.ts
@@ -1,3 +1,5 @@
+import { addColorAlpha } from "./color";
+
 export default function useTheme(theme: string) {
   if (document.body.classList.contains(theme)) {
     document.body.classList.remove(theme);
@@ -5,3 +7,92 @@ export default function useTheme(theme: string) {
     document.body.classList.add(theme);
   }
 }
+
+export type ThemeTypes = {
+  variable: string;
+  value: string;
+};
+
+export const applyTheme = (themes: Array<ThemeTypes> = []) => {
+  const rootStyle = document.getElementsByTagName("body")[0].style;
+  themes.forEach((item, index) => {
+    const oldValue = rootStyle.getPropertyValue(item.variable);
+    if (!oldValue) {
+      // add new variable
+      console.log(
+        `[ASoul] Theme: Add new variable ${item.variable} : ${item.value}`
+      );
+    } else if (oldValue !== item.value) {
+      // overwrite existing variable
+      console.log(
+        `[ASoul] Theme: Overwrite variable ${item.variable} : ${item.value}`
+      );
+    }
+    rootStyle.setProperty(item.variable, item.value);
+  });
+};
+
+export const generateDerivedCSSVariables = (theme: ThemeTypes) => {
+  let arr: ThemeTypes[] = [];
+  if (theme.variable === "--asoul-theme-color") {
+    arr.push(theme);
+    [90, 80, 70, 60, 50, 40, 30, 20, 10].forEach((alpha) => {
+      arr.push({
+        variable: `--asoul-theme-color-${alpha}`,
+        value: addColorAlpha(theme.value, alpha),
+      });
+    });
+  }
+
+  if (theme.variable === "--asoul-primary-color") {
+    arr.push(theme);
+    [12].forEach((alpha) => {
+      arr.push({
+        variable: `--asoul-primary-color-${alpha}`,
+        value: addColorAlpha(theme.value, alpha),
+      });
+    });
+  }
+
+  if (theme.variable === "--asoul-secondary-color") {
+    arr.push(theme);
+    [90, 80, 30].forEach((alpha) => {
+      arr.push({
+        variable: `--asoul-secondary-color-${alpha}`,
+        value: addColorAlpha(theme.value, alpha),
+      });
+    });
+  }
+
+  if (theme.variable === "--asoul-sub-color") {
+    arr.push(theme);
+    [90, 80, 60].forEach((alpha) => {
+      arr.push({
+        variable: `--asoul-sub-color-${alpha}`,
+        value: addColorAlpha(theme.value, alpha),
+      });
+    });
+  }
+
+  if (theme.variable === "--asoul-invert-color") {
+    arr.push(theme);
+    [30].forEach((alpha) => {
+      arr.push({
+        variable: `--asoul-invert-color-${alpha}`,
+        value: addColorAlpha(theme.value, alpha),
+      });
+    });
+  }
+
+  return arr;
+};
+
+export const generateAllDerivedCSSVariables = (
+  themes: Array<ThemeTypes> = []
+) => {
+  let arr: ThemeTypes[] = [];
+  themes.forEach((theme) => {
+    arr.push(...generateDerivedCSSVariables(theme));
+  });
+  return arr;
+};
